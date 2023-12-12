2595e6db0998e0e32faef8c15d514f56ade34116
Amantay21 <amanatayabdilov21@gamil.com>
2023-12-12 18:58:24 +0600

    добавиил функцию редактирования

--- products_store/webapp/urls.py
+++ products_store/webapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from webapp.views import index_view, delete_product, create_product, product_view, create_category, category_view, \
-    product_delete_view
+    product_delete_view, update_product_view
 
 urlpatterns = [
     path('', index_view, name='index'),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('category/add/', create_category, name='category_create'),
     path('category/<int:pk>', category_view, name='category_view'),
     path('product/<int:pk>/delete', product_delete_view, name='product_delete'),
+    path('product/<int:pk>/update', update_product_view, name='product_update'),
 ]
